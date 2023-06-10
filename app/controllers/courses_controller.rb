fd83ef79bad8c6f93349ad31008a6b5efe4431ae
Shubham Gupta <117919046+sguptaglooko@users.noreply.github.com>
2023-06-10 23:45:19 +0530

    assignment changes

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,22 @@
+class CoursesController < ApplicationController
+  def index
+    courses = Course.includes(:tutors)
+    render json: courses, include: :tutors
+  end
+
+  def create
+    course = Course.new(course_params)
+    if course.save
+      render json: course, include: :tutors, status: :created
+    else
+      render json: course.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :duration, tutors_attributes: [:name, :specialization])
+  end
+end
+
